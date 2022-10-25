8e4c2e925c60579f61b13db22242dbf97d53a16c
pung8146 <pung8146@naver.com>
2022-10-25 16:27:44 +0900

    :art: style : footer 부분 CSS 수정입니다. #4

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
 import Footer from './components/Footer';
 import Main from './Main';
 import HeaderBefore from './components/HeaderBefore';
 
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+`;
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <HeaderBefore />
       <Main />
       <Footer />
