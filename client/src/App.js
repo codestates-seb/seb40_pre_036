9a98db70c885fb0c37a35d6cba24a7eb596592ce
ssunip <ssunip99@gmail.com>
2022-10-26 09:24:35 +0900

    :sparkles: feat: header css 수정 #4

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import React from 'react';
 import Footer from './components/Footer';
 import Main from './Main';
 import HeaderBefore from './components/HeaderBefore';
+// import HeaderAfter from './components/HeaderAfter';
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+* {
+  box-sizing: border-box;
+}
+`;
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <HeaderBefore />
+      {/* <HeaderAfter /> */}
       <Main />
       <Footer />
     </div>
