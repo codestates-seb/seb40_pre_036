3f1dc16201aeb0cc5ec9be9f0de36185ad15ee2c
zirryo <jh04187@gmail.com>
2022-10-26 16:11:58 +0900

    :sparkles: feat: QuestionVote Entity 구현 및 Question 수정 #6

--- server/src/main/java/soybeanoil/stackoverflowClone/question/entity/Question.java
+++ server/src/main/java/soybeanoil/stackoverflowClone/question/entity/Question.java
@@ -3,7 +3,9 @@ package soybeanoil.stackoverflowClone.question.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import soybeanoil.stackoverflowClone.answer.entity.Answer;
 import soybeanoil.stackoverflowClone.audit.Auditable;
+import soybeanoil.stackoverflowClone.user.entity.User;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -28,20 +30,22 @@ public class Question extends Auditable {
     @Column(length = 20, nullable = false)
     private QuestionStatus questionStatus = QuestionStatus.QUESTION_EXIST;
 
+    @ManyToOne
+    @Column(name = "QUESTION_WRITER")
+    @JoinColumn(name = "USER_ID")
+    private User user;
 
-//    @ManyToOne
-//    @Column(name = "writer")
-//    @JoinColumn(name = "USER_ID")
-//    private User user;
-//
-//    @OneToMany(mappedBy = "question",cascade = CascadeType.PERSIST)
-//    private List<Answer> answers = new ArrayList<>();
+    @OneToMany(mappedBy = "question", cascade = CascadeType.PERSIST)
+    private List<QuestionVote> questionVotes = new ArrayList<>();
 
     @OneToMany(mappedBy = "question",cascade = CascadeType.PERSIST)
-    private List<Tag> questionTags = new ArrayList<>();
+    private List<Answer> answers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "question",cascade = CascadeType.PERSIST)
+    private List<Tag> tags = new ArrayList<>();
 
     @Column(length = 5, nullable = false)
-    private Integer vote = 0;
+    private Integer votes = 0; // questionVote 를 스트림으로 votes 값 합계
 
     public enum QuestionStatus {
         QUESTION_EXIST("존재하는 질문"),
