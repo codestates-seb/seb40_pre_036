7d014222d55c635dfa8696bdef2420528e56e493
heoseongeun <rhdwnsla1223@naver.com>
2022-10-26 15:25:21 +0900

    :sparkles: feat: User mapper, ResponseDto, PageInfo 구현 #6

---
+++ server/src/main/java/soybeanoil/stackoverflowClone/user/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package soybeanoil.stackoverflowClone.user.mapper;
+
+import soybeanoil.stackoverflowClone.user.entity.User;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    List<UserResponseDto> usersToUserResponseDtos(List<User> users);
+
+    default User userPostDtoToUser(UserPostDto userPostDto) {
+        User user = new User();
+        user.setDisplayName(userPostDto.getDisplayName());
+        user.setEmail(userPostDto.getEmail());
+        user.setPassword(userPostDto.getPassword());
+        return user;
+    }
+
+    default UserResponseDto userToUserResponseDto(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setDisplayName(user.getDisplayName());
+        userResponseDto.setEmail(user.getEmail());
+        userResponseDto.setUserStatus(user.getUserStatus());
+        return userResponseDto;
+    }
+}
