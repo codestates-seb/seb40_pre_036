28095c6e2b2e14c3c815d032b976324e63d32482
uxolrv <uxolrv@gmail.com>
2022-10-26 10:52:51 +0900

    :art: style: Nav CSS 수정 #4

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -7,47 +7,119 @@ const Navbar = styled.nav`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  width: 20%;
-  border-right: 1px solid;
-  margin-left: 15px;
+  justify-content: flex-start;
+  box-sizing: border-box;
+  width: 17%;
+  padding-top: 25px;
+  padding-left: 5%;
+  border-right: 1px solid rgb(216, 217, 220);
+  font-size: 13px;
+  color: rgb(83, 89, 95);
 `;
-const H1 = styled.div`
-  text-decoration: none;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  max-width: 100%;
-  font-weight: 300;
-  margin: 5px 0;
+
+const Home = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 11px 0 11px 8px;
   cursor: pointer;
   &:hover {
-    background-color: grey;
+    color: rgb(12, 13, 14);
   }
+  &.clicked {
+    background-image: linear-gradient(to right, rgb(241, 242, 243) 98.5%, rgb(229, 136, 61) 1.5%);
+    font-weight: bold;
+  }
+`;
+
+const PublicLists = styled.ul`
+  box-sizing: border-box;
+  margin: 7px 0 0 0;
 `;
 
 const Li = styled.li`
-  text-decoration: none;
-  display: flex;
-  margin: 10px -15px 10px 15px;
+  list-style: none;
+  display: inline-block;
+  box-sizing: border-box;
   width: 100%;
-  font-size: 0.7rem;
+  padding: 11px 0 11px 8px;
+  margin: 0;
+  color: rgb(83, 89, 95);
+  font-size: 13px;
   cursor: pointer;
-  &:hover {
-    background-color: grey;
+
+  &.public {
+    padding: 8px;
+    font-size: 11px;
+    color: rgb(108, 115, 123);
+  }
+
+  &:hover:not(.public) {
+    color: rgb(12, 13, 14);
+  }
+
+  &.no-icon {
+    padding-left: 30px;
+  }
+
+  /* &.questions {
+    background-color: ${props => (props.click === ' Questions' ? 'rgb(241, 242, 243)' : '')};
+  }
+
+  &.tags {
+    background-color: ${props => (props.click === 'Tags' ? 'rgb(241, 242, 243)' : '')};
+  }
+
+  &.users {
+    background-color: ${props => (props.click === 'Users' ? 'rgb(241, 242, 243)' : '')};
+  }
+
+  &.companies {
+    background-color: ${props => (props.click === 'Companies' ? 'rgb(241, 242, 243)' : '')};
+  } */
+
+  &.clicked {
+    background-image: linear-gradient(to right, rgb(241, 242, 243) 98.5%, rgb(229, 136, 61) 1.5%);
+    background-color: rgb(241, 242, 243);
+    font-weight: bold;
   }
 `;
 
+const Icon = styled.span`
+  margin-right: 5px;
+  font-size: 16px;
+`;
+
 function Nav() {
+  const [clicked, setClicked] = useState();
+
+  const onClick = useCallback(e => {
+    const text = e.target.innerText;
+    setClicked(text);
+  }, []);
+
   return (
     <Navbar>
-      <H1>Home</H1>
-      <H1>Public</H1>
-      <Li>
-        <FontAwesomeIcon icon={faEarthAmericas} /> Question
-      </Li>
-      <Li>Tags</Li>
-      <Li>Users</Li>
-      <Li>Companies</Li>
+      <Home className={clicked === 'Home' ? 'clicked' : ''} onClick={onClick}>
+        Home
+      </Home>
+      <PublicLists>
+        <Li className="public">PUBLIC</Li>
+        <Li className={clicked === 'Questions' ? 'clicked' : ''} onClick={onClick}>
+          <Icon>
+            <FontAwesomeIcon icon={faEarthAmericas} className="icon" />
+          </Icon>
+          Questions
+        </Li>
+        <Li className={clicked === 'Tags' ? 'no-icon clicked' : 'no-icon'} onClick={onClick}>
+          Tags
+        </Li>
+        <Li className={clicked === 'Users' ? 'no-icon clicked' : 'no-icon'} onClick={onClick}>
+          Users
+        </Li>
+        <Li className={clicked === 'Companies' ? 'no-icon clicked' : 'no-icon'} onClick={onClick}>
+          Companies
+        </Li>
+      </PublicLists>
     </Navbar>
   );
 }
