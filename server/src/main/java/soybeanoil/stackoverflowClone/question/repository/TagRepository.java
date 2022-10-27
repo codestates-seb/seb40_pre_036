421baa751a8a308ae8c281727cfafeaddfa94724
zirryo <jh04187@gmail.com>
2022-10-27 17:41:59 +0900

    :sparkles: feat: QuestionController, QuestionRepository 구현 #17

---
+++ server/src/main/java/soybeanoil/stackoverflowClone/question/repository/TagRepository.java
@@ -0,0 +1,18 @@
+package soybeanoil.stackoverflowClone.question.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import soybeanoil.stackoverflowClone.question.entity.Tag;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    // 특정 QuestionId 의 모든 태그를 찾아서 리스트로 불러옴
+    @Query(value = "select t from Tag t where t.question.questionId = :questionId")
+    List<Tag> findAllByQuestionId(@Param("questionId") long questionId);
+
+    Optional<Tag> findByTagName(String tagName);
+}
