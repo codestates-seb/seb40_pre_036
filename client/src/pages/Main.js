6c1b090a61383c8a74046be142ce4fe70e318bc3
uxolrv <uxolrv@gmail.com>
2022-10-27 11:04:50 +0900

    :sparkles: feat: 질문 목록 페이지 구현 #11

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,17 +2,46 @@ import React from 'react';
 import styled from 'styled-components';
 import Aside from '../components/Aside';
 import Nav from '../components/Nav';
-import Content from '../components/Content';
+// import Content from '../components/Content';
+// import QuestionsHeader from '../components/QuestionsHeader';
+import QuestionsList from '../components/QuestionList';
+import QuestionsHeader from '../components/QuestionsHeader';
 
 const Allcontent = styled.div`
   display: flex;
+`;
+
+// li 태그를 담을 ul태그
+const ListContainer = styled.ul`
+  /* width: 751px; */
+  display: flex;
+  flex-direction: column;
+  color: #232629;
+  font-size: 13px;
 `;
 
 function Main() {
   return (
     <Allcontent>
       <Nav />
-      <Content />
+      <ListContainer>
+        <QuestionsHeader />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+        <QuestionsList />
+      </ListContainer>
       <Aside />
     </Allcontent>
   );
