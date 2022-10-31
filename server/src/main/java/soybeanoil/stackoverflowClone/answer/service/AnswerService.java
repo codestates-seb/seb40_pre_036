4c9ce4fd12cbceb80c0b8d25e0b072875043ec64
youngjun kim <108052178+kyj4327@users.noreply.github.com>
2022-10-31 15:35:16 +0900

    ✨ feat: Service 구현 #15

---
+++ server/src/main/java/soybeanoil/stackoverflowClone/answer/service/AnswerService.java
@@ -0,0 +1,85 @@
+package soybeanoil.stackoverflowClone.answer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import soybeanoil.stackoverflowClone.answer.entity.Answer;
+import soybeanoil.stackoverflowClone.answer.entity.AnswerVote;
+import soybeanoil.stackoverflowClone.answer.repository.AnswerRepository;
+import soybeanoil.stackoverflowClone.exception.BusinessLogicException;
+import soybeanoil.stackoverflowClone.exception.ExceptionCode;
+import soybeanoil.stackoverflowClone.question.entity.Question;
+import soybeanoil.stackoverflowClone.user.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class AnswerService {
+    @Autowired
+    private AnswerVoteService answerVoteService;
+    private AnswerRepository answerRepository;
+
+    public Answer createAnswer(Answer answer) {  //  SOF-A-001 답변 작성
+        return answerRepository.save(answer);
+    }
+
+    public Answer modifyAnswer(Answer answer) {  // SOF-A-002 답변 수정
+        Answer findAnswer = findVerifiedAnswer(answer.getAnswerId());
+
+        return createAnswer(findAnswer);
+    }
+    public Answer findAnswer(long answerId) {
+        Answer findAnswer = findVerifiedAnswer(answerId);
+        // 별도의 태그 처리 로직이 필요할 경우 이곳에 작성
+        return findAnswer;
+    }
+    private Answer findVerifiedAnswer(Long answerId) {  // SOF-A-002 답변 맞는지 확인
+        Optional<Answer> optionalAnswer = answerRepository.findById(answerId);
+        Answer findAnswer = optionalAnswer.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.ANSWER_NOT_FOUND));
+
+        return findAnswer;
+    }
+
+
+    public void deleteAnswer(long answerId) {  // SOF-A-003 답변 삭제  => 작성한 answerId 기준
+        Answer answer = findVerifiedAnswer(answerId);
+        answerRepository.delete(answer);
+
+    }
+
+//    public Answer voteCount(int vote,int count) {  // SOF-A-004 upvote 세기
+//
+//    }
+
+
+    private void VerifiedNoAnswer(Page<Answer> findAllAnswer) throws BusinessLogicException { //ANSWER_EXIST List=0 일떄 예외처리
+        if (findAllAnswer.getTotalElements() == 0) {
+            throw new BusinessLogicException(ExceptionCode.ANSWER_NOT_FOUND);
+        }
+    }
+    public User findAnswerUser(long answerId){
+        Answer findAnswer = findVerifiedAnswer(answerId);
+        return findAnswer.getUser();
+    }
+    public Page<Answer> findAnswers(Question question, int answerPage, int answerSize, String answerSort) throws BusinessLogicException{
+        Page<Answer> findAllAnswer = answerRepository.findAllByQuestionAndAnswerStatus(
+                PageRequest.of(answerPage-1,answerSize, Sort.by(answerSort).descending()),
+                question, Answer.AnswerStatus.ANSWER_EXIST);
+        VerifiedNoAnswer(findAllAnswer);
+
+        return findAllAnswer;
+        }
+// 투표
+    public void refreshVotes(User userid) {
+        Answer answer = answerRepository.findById(userid.getUserId()).get();
+        answer.setVotes(answerVoteService.getVoteValue(new AnswerVote()));
+        answerRepository.save(answer);
+    }
+
+}
+
