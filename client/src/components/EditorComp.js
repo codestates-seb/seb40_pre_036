f9b222133e5b65574a1fecd6a9850a7ea6774e0f
ssunip <ssunip99@gmail.com>
2022-10-31 23:10:56 +0900

    :art: style: md editor css 수정 #46

--- client/src/components/EditorComp.js
+++ client/src/components/EditorComp.js
@@ -5,43 +5,33 @@ import rehypeSanitize from 'rehype-sanitize';
 
 const Container = styled.div`
   width: 800px;
-  border: 1px solid #babfc4;
+  height: 500px;
+  border: 1px solid transparent;
   border-radius: 5px;
 
-  &:focus-within {
-    outline: none;
-    border-color: hsl(206deg 100% 52%);
-    box-shadow: 0px 0px 0px 5px #e1ecf4;
-  }
-  li {
-    list-style: inside;
-    color: red;
+  .w-md-editor {
+    border: 1px solid #d0d7de;
+    box-shadow: none;
+
+    &:focus-within {
+      border-color: hsl(206deg 100% 52%);
+      box-shadow: 0px 0px 0px 5px #e1ecf4;
+    }
   }
 `;
 
 function EditorComp() {
   const [content, setContent] = useState('');
-  // const [viewContent, setViewContent] = useState([]);
-  // const getValue = e => {
-  //   const { name, value } = e.target;
-  //   setMovieContent({
-  //     ...movieContent,
-  //     [name]: value,
-  //   });
-  //   console.log(movieContent);
-  // };
-  // const onClick = {() => {
-  //   setViewContent(viewContent.concat({...questionContent}));
-  // }}
+
   return (
     <Container>
       <MDEditor
-        className="list"
         value={content}
         onChange={setContent}
         previewOptions={{
           rehypePlugins: [[rehypeSanitize]],
         }}
+        height={498}
       />
     </Container>
   );
