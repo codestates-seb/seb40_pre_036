8cfa9c6bef6ea1ec1f6bc8c9c3c4b3f75321f020
Jihyo <jh04187@gmail.com>
2022-10-31 19:04:51 +0900

    :sparkles: feat: Exception code 추가 #48

--- server/src/main/java/soybeanoil/stackoverflowClone/exception/ExceptionCode.java
+++ server/src/main/java/soybeanoil/stackoverflowClone/exception/ExceptionCode.java
@@ -4,7 +4,11 @@ import lombok.Getter;
 
 public enum ExceptionCode {
     USER_NOT_FOUND(404, "User not found"),
-    USER_EXISTS(409, "User exists");
+    USER_EXISTS(409, "User exists"),
+    ACCESS_DENIED_USER(403, "Access Denied User"),
+    QUESTION_EXIST(409, "Question Exists"),
+    TAG_EXIST(409, "Tag Exists"),
+    QUESTION_NOT_FOUND(404, "Question Not Found");
 
     @Getter
     private int status;
