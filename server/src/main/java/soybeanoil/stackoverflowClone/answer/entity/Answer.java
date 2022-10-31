bb5830f38eee10ce531d9e15b7ecc981435a9dcd
youngjun kim <108052178+kyj4327@users.noreply.github.com>
2022-10-31 19:21:13 +0900

    :bug: fix: AnswerEntity 연관관계 매핑 수정  #6

--- server/src/main/java/soybeanoil/stackoverflowClone/answer/entity/Answer.java
+++ server/src/main/java/soybeanoil/stackoverflowClone/answer/entity/Answer.java
@@ -15,7 +15,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor  // (생성자 자동 생성) 매개변수 없는 생성자 자동 생성
 @Getter
 @Setter
-@Entity(name = "answer")
+@Entity
+@Table(name = "answer")
 public class Answer extends Auditable {
 
     @Id
@@ -25,17 +26,14 @@ public class Answer extends Auditable {
     @Column(nullable = false, columnDefinition = "TEXT")  //컬럼을 text 로 설정하여 데이터를 추출
     private String answerContent; // content -> Q & A 따로 구별
 
-    @OneToOne // 1:1매핑 => answer writer <-> user id
-    @JoinColumn(name="USER_ID")
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
     private User user;  // 질문자 user id 엔티티랑 묶기
 
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "QUESTION_ID")
     private Question question;   // Question 에 붙은 답변 => Question 연결
-
-    @Column(nullable = false)
-    private int vote;
 
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
@@ -47,6 +45,7 @@ public class Answer extends Auditable {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, name = "STATUS")
     private AnswerStatus answerStatus = AnswerStatus.ANSWER_EXIST;
+
     public enum AnswerStatus {
         ANSWER_NOT_EXIST("존재하지 않는 답변"),
         ANSWER_EXIST("존재하는 답변");
@@ -58,5 +57,10 @@ public class Answer extends Auditable {
             this.status = status;
         }
     }
+    @Column(nullable = false)
+    private long vote;
 
+    public void setVotes(long votes) {
+        this.vote = votes;
+    }
 }
