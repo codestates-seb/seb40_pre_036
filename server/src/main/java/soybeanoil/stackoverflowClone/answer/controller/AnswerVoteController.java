f1108b1d49289131a55b5f5767403c4f10e3d016
youngjun kim <108052178+kyj4327@users.noreply.github.com>
2022-11-03 17:03:17 +0900

    :bug: fix: @CrossOrigin 추가 #44

--- server/src/main/java/soybeanoil/stackoverflowClone/answer/controller/AnswerVoteController.java
+++ server/src/main/java/soybeanoil/stackoverflowClone/answer/controller/AnswerVoteController.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @Validated
 @RequestMapping("/answer")
